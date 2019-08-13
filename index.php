6f4f817c76c159c16b7cbe80c5b86bbb71c244ae
akmj <kuncorochandra17@gmail.com>
2019-08-13 13:52:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
 <body>
 <h3>test</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptate totam odit sint dolore cumque ipsum perspiciatis harum! Soluta expedita sit aperiam blanditiis, esse repellendus porro praesentium aspernatur odit ullam.</p>
+<ul>
+	<li> satu</li>
+	<li> dua </li>
+</ul>
 </body>
 </html>
