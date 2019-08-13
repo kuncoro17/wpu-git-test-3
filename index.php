b5cf67cec8e5286bea4f64a58982b2c39f24dce7
akmj <kuncorochandra17@gmail.com>
2019-08-13 13:40:04 +0700

    menambahkan paragrap

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 <h3>test</h3>
-<p></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptate totam odit sint dolore cumque ipsum perspiciatis harum! Soluta expedita sit aperiam blanditiis, esse repellendus porro praesentium aspernatur odit ullam.</p>
 </body>
 </html>
